24a4345b0a7e3d3c426d0dc94d90b22b1f216bbf
Santiago Torres G <thdealer@gmail.com>
2020-01-28 17:33:49 -0500

    handling state in login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ password, email });
+    // createBook(newBook);
+    // return newBook;
+  };
+
   return (
     <div>
-      LOGIN OK
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          <input
+            id="email"
+            label="email"
+            type="text"
+            name="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="E-Mail"
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            id="password"
+            label="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Password"
+            required
+          />
+        </label>
+        <button type="submit" style={undefined}>ADD BOOK</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
