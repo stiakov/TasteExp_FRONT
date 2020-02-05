f539b7ddcf030bbd53c91d17cd56097bebe9466c
Santiago Torres G <thdealer@gmail.com>
2020-02-04 23:27:29 -0500

    deleting console.log()

--- src/redux/fetchFavoriteActions.js
+++ src/redux/fetchFavoriteActions.js
@@ -7,16 +7,13 @@ const BASE_URL = 'http://localhost:3000/v1';
 
 const FETCH_MY_FAVS = 'FETCH_MY_FAVS';
 
-const fetchMyFavs = ({ headers }) => dispatch => {
-  console.log('user-> ', headers);
-  return (
-    axios.get(`${BASE_URL}/favorites/user`, { headers })
-      .then((response) => dispatch({
-        type: FETCH_MY_FAVS,
-        favorite: response.data,
-      }), error => errorLogger(error, dispatch))
-  );
-};
+const fetchMyFavs = ({ headers }) => dispatch => (
+  axios.get(`${BASE_URL}/favorites/user`, { headers })
+    .then((response) => dispatch({
+      type: FETCH_MY_FAVS,
+      favorite: response.data,
+    }), error => errorLogger(error, dispatch))
+);
 
 export {
   FETCH_MY_FAVS,
