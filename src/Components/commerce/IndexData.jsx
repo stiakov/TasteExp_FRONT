ffc4d2782c41fa95a5be9acf07091697f9dd3d1b
Santiago Torres G <thdealer@gmail.com>
2020-02-05 17:47:25 -0500

    adding components for show one commerce

---
+++ src/Components/commerce/IndexData.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const IndexData = (props) => {
+  return (
+    <div className="ui centered card">
+      <div className="content">
+        <div className="center aligned header">Cute Dog</div>
+        <div className="meta">2 days ago</div>
+        <div className="description">
+          <p>{props.description}.</p>
+          <div className="ui list">
+            <div className="item">
+              <i className="users icon" />
+              <div className="content">
+                  Semantic UI
+              </div>
+            </div>
+            <div className="item">
+              <i className="marker icon" />
+              <div className="content">
+                  New York, NY
+              </div>
+            </div>
+            <div className="item">
+              <i className="mail icon" />
+              <div className="content">
+                <a href="mailto:jack@semantic-ui.com">jack@semantic-ui.com</a>
+              </div>
+            </div>
+            <div className="item">
+              <i className="linkify icon" />
+              <div className="content">
+                <a href="http://www.semantic-ui.com">semantic-ui.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="extra content">
+        <i className="check icon" />
+                121 Votes
+      </div>
+    </div>
+  )
+};
+
+export default IndexData;
---
+++ src/Pages/Commerce.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import giphy from '../images/giphy1.gif';
+import IndexData from '../Components/commerce/IndexData';
+
+const Commerce = () => {
+  return (
+    <div>
+      <div className="ui hidden divider"></div>
+      <h1 className="header commerce-name">Commerce Name</h1>
+      <div className="ui two column doubling stackable grid container">
+        <div className="twelve wide column">
+          <img className="ui fluid image" src={giphy} alt="comm-image"/>
+        </div>
+        <div className="four wide column">
+          
+          <IndexData
+            name={}
+            description={}
+            country={}
+            state={}
+            city={}
+            address={}
+            landline={}
+            mobile={}
+            email={}
+            website={}
+            instagram={}
+            commerceType={}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Commerce;
